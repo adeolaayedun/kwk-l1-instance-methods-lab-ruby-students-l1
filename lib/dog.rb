7e1b3a33da73458c82667a7935b87f6bb23d38f3
adeolaayedun <deola.ayedun@gmail.com>
2018-06-07 19:33:51 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
 # dog.rb
+class Dog 
+
+  def initialize (bark)
+    @bark = bark
+  end 
+
+  def bark
+    @bark 
+    puts "woof!"
+  end
+
+  def sit (sit)
+    @sit = sit
+  end
+
+  def sit 
+    @sit
+    puts "The dog is sitting."
+  end 
+end
+    
